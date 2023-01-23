88cf0e2fb5c69f37f701518866fa3da221ca39f3
DmitriiYaganin <dmitrij.yaganin@mail.ru>
2023-01-23 11:15:38 +0400

    Update HW31

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,5 +9,5 @@ def access_token(client, django_user_model):
     django_user_model.objects.create_user(username=username, password=password, role="moderator")
 
     response = client.post("/user/token/", {"username": username, "password": password},
-                           content_type="application.json")
+                           content_type="application/json")
     return response.data["access"]
